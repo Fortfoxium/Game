3f806a188faadd90535d8c931b494982e66a477b
Vlad <animation1973@mail.ru>
2021-07-11 13:18:50 +0200

    Меню готова

---
+++ Game/Assets/MAINMENU.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MAINMENU : MonoBehaviour
+{
+  public void PlayGame()
+    { 
+        SceneManager.Loadscene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+ 
+
+}
